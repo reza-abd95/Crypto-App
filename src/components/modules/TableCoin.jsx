30bb6a671a85c698407ae041661bf0faeba147be
Reza Abedi <reza.abd95@yahoo.com>
2024-01-17 18:38:10 +0330

    feat(crypto api) : added api.js

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 function TableCoin({ coins }) {
+  console.log(coins);
   return <div>TableCoin</div>;
 }
 
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
+import { getListCoin } from "../../services/cryptoApi";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&x_cg_demo_api_key=CG-o4Vovb1bZtxa1vSiZRUjkKLs"
-    )
+    fetch(getListCoin())
       .then((res) => res.json())
       .then((json) => setCoins(json));
   }, []);
