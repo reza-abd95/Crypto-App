35185f9c1027848772061ecc6ca357dd51882d9c
Reza Abedi <reza.abd95@yahoo.com>
2024-01-20 21:30:20 +0330

    feat(TableCoin) : added table of coins

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,8 +1,48 @@
 import React from "react";
+import chartup from "../../assets/chart-up.svg";
+import chartdown from "../../assets/chart-down.svg";
 
 function TableCoin({ coins }) {
-  console.log(coins);
-  return <div>TableCoin</div>;
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total Volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.id}>
+              <td>
+                <div>
+                  <img src={coin.image} alt="" />
+                  <span>{coin.symbol.toUpperCase()}</span>
+                </div>
+              </td>
+              <td>{coin.name}</td>
+              <td>${coin.current_price.toLocaleString()}</td>
+              <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+              <td>{coin.total_volume}</td>
+              <td>
+                <img
+                  src={
+                    coin.price_change_percentage_24h > 0 ? chartup : chartdown
+                  }
+                  alt={coin.name}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default TableCoin;
