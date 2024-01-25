85209756100a3a7dbb819dfe92f64377a1fd20c7
Reza Abedi <reza.abd95@yahoo.com>
2024-01-26 02:09:50 +0330

    feat(search) : added styles to searchbox

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -41,36 +41,50 @@ function Search({ currency, setCurrency }) {
     return () => controller.abort();
   }, [text]);
   return (
-    <div className="text-black">
+    <div className=" mt-12 relative">
       <input
+        className=" w-80 h-14 p-3 text-lg bg-[#23242e] border-none rounded-md focus:outline-none"
         type="text"
         placeholder="Search"
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+      <select
+        className="h-14 px-2 py-3 ml-5 text-lg bg-[#23242e] border-none rounded-md focus:outline-none"
+        value={currency}
+        onChange={(e) => setCurrency(e.target.value)}
+      >
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
-      <div>
-        {isLoading && (
-          <RotatingLines
-            height="50"
-            width="50"
-            strokeColor="red"
-            strokeWidth="2"
-          />
-        )}
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id} className="text-white flex">
-              <img src={coin.thumb} alt={coin.name} />
-              <p>{coin.name}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {(!!coins.length || isLoading) && (
+        <div className=" absolute text-center top-20 w-80 h-96 rounded-md overflow-y-scroll p-5 bg-[#18181c] border-solid border-2 border-[#22262e] scrollbar">
+          {isLoading && (
+            <RotatingLines
+              height="50"
+              width="50"
+              strokeColor="red"
+              strokeWidth="2"
+            />
+          )}
+          <ul>
+            {coins.map((coin) => (
+              <li
+                key={coin.id}
+                className="text-white flex mb-4 pb-[5px] border-b-2 border-solid border-[#22262e]"
+              >
+                <img
+                  className=" mr-2 w-7 h-7"
+                  src={coin.thumb}
+                  alt={coin.name}
+                />
+                <p>{coin.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
